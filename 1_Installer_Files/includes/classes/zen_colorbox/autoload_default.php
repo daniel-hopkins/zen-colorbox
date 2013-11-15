ab0d89c52db2e0ec274977cc1677dfa7e41b62f9
Daniel Hopkins <d@niestudio.com>
2013-11-14 21:58:40 -0800

    add responsive colorbox update

--- 1_Installer_Files/includes/classes/zen_colorbox/autoload_default.php
+++ 1_Installer_Files/includes/classes/zen_colorbox/autoload_default.php
@@ -14,4 +14,23 @@ jQuery(function($) {
   // Disable Colobox on main reviews page image
   $("#productMainImageReview a").removeAttr("rel");
 });
+// Begin Make ColorBox responsive
+jQuery.colorbox.settings.maxWidth  = '95%';
+jQuery.colorbox.settings.maxHeight = '95%';
+
+// ColorBox resize function
+var resizeTimer;
+function resizeColorBox() {
+  if (resizeTimer) clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+    if (jQuery('#cboxOverlay').is(':visible')) {
+      jQuery.colorbox.load(true);
+    }
+  }, 300);
+}
+
+// Resize ColorBox when resizing window or changing mobile device orientation
+jQuery(window).resize(resizeColorBox);
+window.addEventListener("orientationchange", resizeColorBox, false);
+// End Make ColorBox responsive
 //--></script>
