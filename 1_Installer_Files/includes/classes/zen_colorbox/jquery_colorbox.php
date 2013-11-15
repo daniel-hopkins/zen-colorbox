f486e1c2f6412d2c0360f0bea74def55c7118576
Daniel Hopkins <d@niestudio.com>
2013-11-14 21:39:31 -0800

    apply @divavocals update to auto load jQuery 1.10.2

--- 1_Installer_Files/includes/classes/zen_colorbox/jquery_colorbox.php
+++ 1_Installer_Files/includes/classes/zen_colorbox/jquery_colorbox.php
@@ -6,9 +6,8 @@
  * @copyright Copyright 2003-2005 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: slimbox.php 2012-04-30 niestudio $
+ * @version $Id: jquery_colorbox.php 2012-04-30 niestudio $
  */
-echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>';
 echo '<script type="text/javascript" src="' . $template->get_template_dir('.js', DIR_WS_TEMPLATE, $current_page_base, 'jscript') . '/jquery.colorbox-min.js"></script>';
 ?>
 <script language="javascript" type="text/javascript">
---
+++ 1_Installer_Files/includes/templates/YOUR_TEMPLATE/jscript/jscript_jquery.min.php
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
