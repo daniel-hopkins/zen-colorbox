b98b72178a7a601e6de1b9c2691fc79b57523539
DivaVocals <divawebdzgn@aol.com>
2014-12-24 09:42:24 -0800

    Minor fix to jQuery file

--- 1_Installer_Files/includes/templates/YOUR_TEMPLATE/jscript/jscript_jquery.min.php
+++ 1_Installer_Files/includes/templates/YOUR_TEMPLATE/jscript/jscript_jquery.min.php
@@ -1,5 +1,7 @@
+<?php
 //Load jQuery ONLY if jQuery has not been loaded. This is to support versions of Zen Cart prior to v1.5.4 because jQuery was not part of core code
-//Also prevent jQuery from loading again if bundled with a plugin or template package
+//Also want to prevent jQuery from loading again if bundled with a plugin or template package
+?>
 <script type="text/javascript">
 if (typeof jQuery == 'undefined') {
     document.write('script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"><\/script>');
